1e1b63755a77582c69d3f3deaf0b1d47308e8f5e
harryjordancma <112893476+harryjordancma@users.noreply.github.com>
2026-08-12 10:08:06 +0100

    Step 5: make_document

--- model.py
+++ model.py
@@ -84,8 +84,12 @@ def normalize_text(text):
     text = unicodedata.normalize("NFKC", text)
     return " ".join(text.split())
 
-# Step 5 - make_document (not yet solved)
-# TODO: implement
+# Step 5 - make_document
+def make_document(text, source, title):
+    return {"text": text,
+            "source": source,
+            "title": title
+        }
 
 # Step 6 - chunk_fixed_size (not yet solved)
 # TODO: implement
